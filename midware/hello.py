73030cb819731dc1c63d1bcff88593bbba9882ab
Wql2017 <932865549@qq.com>
2021-12-14 07:42:22 +0000

    Changes to be committed: 	modified:   hello.py 	new file:   templates/echo_ui.jinja2  Untracked files: 	../.gitpod.yml

--- midware/hello.py
+++ midware/hello.py
@@ -1,10 +1,34 @@
 from flask import Flask
 from datetime import datetime
+from flask import request
+from flask import render_template
 app = Flask(__name__)
 
+message_list = []
+
+def process_input(request:request):
+   one_message = request.form.get('msg')
+   message_list.append(one_message)
+
+def process_output():
+   output_dict = {}
+   output_dict['messages'] = message_list
+
+   return output_dict
+
+
 @app.route('/')
-def hello_world():
-   return 'Hello World'
+def index():
+   process_input(request)
+   message_list[0] = "这是一个留言板"
+   output_dict = process_output()
+   return render_template('echo_ui.jinja2',bean = output_dict,)
+
+@app.route('/echo',methods=["post","get"])
+def echo_function():
+   process_input(request)
+   output_dict = process_output()
+   return render_template('echo_ui.jinja2',bean = output_dict)
 
 @app.route('/time')
 def print_time():
